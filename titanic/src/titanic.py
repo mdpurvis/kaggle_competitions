5ee106dd5d9a3182ff0c1ad1ec6798da70425061
Ian Stone <ianstone30@gmail.com>
2020-02-08 08:59:59 -0500

    built one hot encoding appender
    col names out of whack, need to adjust

--- titanic/src/titanic.py
+++ titanic/src/titanic.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from sklearn.preprocessing import OneHotEncoder
 
 def identify_blanks(df):
     t = len(df)
@@ -36,4 +37,24 @@ def one_hot_age_gender(df):
     df.loc[((df['Boy'] == 0) & (df['Girl'] == 0) &  (df['Man'] == 0) & (df['Woman'] == 0)) &
            (df['Name'].str.contains('Dr.') & (df['Sex'] == 'male')), ['Man']] = 1
 
+    return df
+
+def split_cabin(df):
+    df.loc[df['Cabin'].notnull(), 'Cabin Prefix'] = df.loc[
+        df['Cabin'].notnull(), 'Cabin'].str[0]
+    df.loc[df['Cabin'].notnull(), 'Cabin Number'] = df.loc[
+        df['Cabin'].notnull(), 'Cabin'].str.extract('(\d+)', expand=False)
+    df['Cabin Number'] = pd.to_numeric(df['Cabin Number']).astype('Int64')
+    return df
+
+def append_one_hot(df, col):
+    df['Embarked'] = df['Embarked'].fillna('')
+    ohe = OneHotEncoder(sparse = False, dtype = int, handle_unknown = 'ignore')
+    data = ohe.fit_transform(df[['Embarked']])
+
+    col_names = [col + ' ' + val for val in list(ohe.categories_)]
+
+    temp_df = pd.DataFrame(data, columns = col_names, index = df.index)
+    temp_df.drop(col + ' ', axis=1, inplace=True)
+    df = df.join(temp_df)
     return df
